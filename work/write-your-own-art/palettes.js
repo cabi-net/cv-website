a67075197bb5874265b1d3115a4c862c1fb6bccf
cabi-net <silataskin@gmail.com>
2026-01-05 21:16:39 -0500

    updates
    too many

---
+++ work/write-your-own-art/palettes.js
@@ -0,0 +1,5 @@
+let palette1 = ["#386641","#6a994e","#a7c957","#f2e8cf","#bc4749"]
+// copy as array from coolors
+
+let palette2 = ["#eae4e9","#fff1e6","#fde2e4","#fad2e1","#e2ece9","#bee1e6","#f0efeb","#dfe7fd","#cddafd"]
+let cherryBlossomBloom = ["#590d22","#800f2f","#a4133c","#c9184a","#ff4d6d","#ff758f","#ff8fa3","#ffb3c1","#ffccd5","#fff0f3"]
